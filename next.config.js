ce57041fb5dc130f6ec890e4ca8ac1d3eaa8439e
chagy <chagy_x_@hotmail.com>
2022-01-21 10:34:09 +0700

    12

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  env: {
+    DB_LOCAL_URI: `mongodb+srv://bookit:4WM4AE1XJHOVLIoy@cluster0.c7cg1.mongodb.net/bookit?retryWrites=true&w=majority`,
+  },
   reactStrictMode: true,
-}
+};
