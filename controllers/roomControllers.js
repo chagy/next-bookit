2f96cd46fb0ca0735f9f9564ef48001f8b6604b5
chagy <chagy_x_@hotmail.com>
2022-01-21 13:12:44 +0700

    18

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -58,4 +58,34 @@ const getSingleRoom = async (req, res) => {
   }
 };
 
-export { allRooms, newRoom, getSingleRoom };
+// Post room detail -> /api/rooms/:id
+const updateRoom = async (req, res) => {
+  try {
+    let room = await Room.findById(req.query.id);
+
+    if (!room) {
+      return res.status(400).json({
+        success: false,
+        error: "Room not found with this ID",
+      });
+    }
+
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export { allRooms, newRoom, getSingleRoom, updateRoom };
