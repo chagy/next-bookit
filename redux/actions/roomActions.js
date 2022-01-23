59380056e68dc39852a8b655bbbe1ee6c60f6a50
chagy <chagy_x_@hotmail.com>
2022-01-23 13:36:03 +0700

    43

--- redux/actions/roomActions.js
+++ redux/actions/roomActions.js
@@ -23,6 +23,23 @@ export const getRooms = (req) => async (dispatch) => {
   }
 };
 
+export const getRoomDetails = (req, id) => async (dispatch) => {
+  try {
+    const { origin } = absoluteUrl(req);
+    const { data } = await axios.get(`${origin}/api/rooms/${id}`);
+
+    dispatch({
+      type: ROOM_DETAILS_SUCCESS,
+      payload: data.room,
+    });
+  } catch (error) {
+    dispatch({
+      type: ROOM_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
 export const clearErrors = () => async (dispatch) => {
   dispatch({
     type: CLEAR_ERRORS,
--- redux/reducers/roomReducers.js
+++ redux/reducers/roomReducers.js
@@ -1,6 +1,8 @@
 import {
   ALL_ROOMS_SUCCESS,
   ALL_ROOMS_FAIL,
+  ROOM_DETAILS_SUCCESS,
+  ROOM_DETAILS_FAIL,
   CLEAR_ERRORS,
 } from "../constants/roomConstants";
 
@@ -14,6 +16,28 @@ export const allRoomsReducer = (state = { rooms: [] }, action) => {
         rooms: action.payload.rooms,
       };
     case ALL_ROOMS_FAIL:
+      return {
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const roomDetailsReducer = (state = { rooms: {} }, action) => {
+  switch (action.type) {
+    case ROOM_DETAILS_SUCCESS:
+      return {
+        room: action.payload,
+      };
+    case ROOM_DETAILS_FAIL:
       return {
         error: action.payload,
       };
