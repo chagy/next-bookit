ddd600112d7c95d50dff3bbf8d1a2afc1d4203c4
chagy <chagy_x_@hotmail.com>
2022-01-23 13:18:37 +0700

    41

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
     DB_LOCAL_URI: `mongodb+srv://bookit:4WM4AE1XJHOVLIoy@cluster0.c7cg1.mongodb.net/bookit?retryWrites=true&w=majority`,
   },
   reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
