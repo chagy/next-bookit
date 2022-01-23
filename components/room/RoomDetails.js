891c75e51f07cc8da6b4a134b7f48583367127e6
chagy <chagy_x_@hotmail.com>
2022-01-23 14:32:22 +0700

    45

--- components/room/RoomDetails.js
+++ components/room/RoomDetails.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 
+import RoomFeatures from "./RoomFeatures";
+
 import { Carousel } from "react-bootstrap";
 
 import { useDispatch, useSelector } from "react-redux";
@@ -57,37 +59,13 @@ const RoomDetails = () => {
         <div className="row my-5">
           <div className="col-12 col-md-6 col-lg-8">
             <h3>Description</h3>
-            <p>
-              We are offering fully furnished apartment with your all basic
-              needs being fulfilled. It consists of 2 king size bed room with a
-              beautiful view in the living room, it also has a fully operational
-              kitchen with microwave and basic crockery. Beside all it has a
-              amazing balcony infront of valley view.
-            </p>
-            <div className="features mt-5">
-              <h3 className="mb-4">Features:</h3>
-              <div className="room-feature">
-                <i className="fa fa-cog fa-fw fa-users" aria-hidden="true" />
-                <p>6 Guests</p>
-              </div>
-              <div className="room-feature">
-                <i className="fa fa-cog fa-fw fa-bed" aria-hidden="true" />
-                <p>2 Beds</p>
-              </div>
-              <div className="room-feature">
-                <i className="fa fa-cog fa-fw fa-bath" aria-hidden="true" />
-                <p>2 Baths</p>
-              </div>
-              <div className="room-feature">
-                <i className="fa fa-cog fa-fw fa-cutlery" aria-hidden="true" />
-                <p>Kitchen</p>
-              </div>
-            </div>
+            <p>{room.description}</p>
+            <RoomFeatures room={room} />
           </div>
           <div className="col-12 col-md-6 col-lg-4">
             <div className="booking-card shadow-lg p-4">
               <p className="price-per-night">
-                <b>$28</b> / night
+                <b>${room.pricePerNight}</b> / night
               </p>
               <button className="btn btn-block py-3 booking-btn">Pay</button>
             </div>
