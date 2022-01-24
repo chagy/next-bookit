c938a6b150fdecd922dc7aae3f22c695261f6cd1
chagy <chagy_x_@hotmail.com>
2022-01-24 15:53:00 +0700

    59

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { useDispatch, useSelector } from "react-redux";
 import { loadUser } from "../../redux/actions/userActions";
+import { signOut } from "next-auth/client";
 
 function Header() {
   const dispatch = useDispatch();
@@ -12,6 +13,10 @@ function Header() {
   useEffect(() => {
     dispatch(loadUser());
   }, [dispatch]);
+
+  const logoutHandler = () => {
+    signOut();
+  };
 
   return (
     <nav className="navbar row justify-content-center sticky-top">
@@ -57,7 +62,12 @@ function Header() {
                   <a className="dropdown-item">Profile</a>
                 </Link>
                 <Link href="/">
-                  <a className="dropdown-item">Logout</a>
+                  <a
+                    className="dropdown-item text-danger"
+                    onClick={logoutHandler}
+                  >
+                    Logout
+                  </a>
                 </Link>
               </div>
             </div>
