b14f2e317852fdd4d222ca80e20f6bda94b615e5
chagy <chagy_x_@hotmail.com>
2022-01-24 11:24:50 +0700

    53

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,16 +15,19 @@ export default NextAuth({
 
         const { email, password } = credentials;
 
+        // Check if email and password is entered
         if (!email || !password) {
           throw new Error("Please enter email or password");
         }
 
+        // Find user in the database
         const user = await User.findOne({ email }).select("+password");
 
         if (!user) {
           throw new Error("Invalid Email or Password");
         }
 
+        // Check if password is correct or not
         const isPasswordMatched = await user.comparePassword(password);
 
         if (!isPasswordMatched) {
@@ -42,7 +45,7 @@ export default NextAuth({
     },
     session: async (session, user) => {
       session.user = user.user;
-      return Promise.resolve();
+      return Promise.resolve(session);
     },
   },
 });
