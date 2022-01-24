7db0fd2482da2a5d90e4ff649a2d1acb25ed84d6
chagy <chagy_x_@hotmail.com>
2022-01-24 11:38:14 +0700

    54

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   env: {
     DB_LOCAL_URI: `mongodb+srv://bookit:4WM4AE1XJHOVLIoy@cluster0.c7cg1.mongodb.net/bookit?retryWrites=true&w=majority`,
+
+    CLOUDINARY_CLOUD_NAME: "chagy",
+    CLOUDINARY_API_KEY: "599531334361122",
+    CLOUDINARY_API_SECRET: "jTLURqLVM5Yavbegp9zECRVmxDE",
   },
   reactStrictMode: true,
   images: {
