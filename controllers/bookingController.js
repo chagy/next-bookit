68dbc126eac113b504f1b53ea47d39f363021de2
chagy <chagy_x_@hotmail.com>
2022-01-26 10:58:03 +0700

    76

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,6 +3,11 @@ import Booking from "../models/booking";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors";
 
+import Moment from "moment";
+import { extendMoment } from "moment-range";
+
+const moment = extendMoment(Moment);
+
 // Create new Booking   =>   /api/auth/register
 const newBooking = catchAsyncErrors(async (req, res) => {
   const {
@@ -22,6 +27,7 @@ const newBooking = catchAsyncErrors(async (req, res) => {
     daysOfStay,
     amountPaid,
     paymentInfo,
+    paidAt: Date.now(),
   });
 
   res.status(200).json({
@@ -66,4 +72,36 @@ const checkRoomBookingsAvailability = catchAsyncErrors(async (req, res) => {
   });
 });
 
-export { newBooking, checkRoomBookingsAvailability };
+// Check booked dates of a room   =>   /api/bookings/check_booked_dates
+const checkBookedDatesOfRoom = catchAsyncErrors(async (req, res) => {
+  const { roomId } = req.query;
+
+  const bookings = await Booking.find({ room: roomId });
+
+  let bookedDates = [];
+
+  const timeDiffernece = moment().utcOffset() / 60;
+
+  bookings.forEach((booking) => {
+    const checkInDate = moment(booking.checkInDate).add(
+      timeDiffernece,
+      "hours"
+    );
+    const checkOutDate = moment(booking.checkOutDate).add(
+      timeDiffernece,
+      "hours"
+    );
+
+    const range = moment.range(moment(checkInDate), moment(checkOutDate));
+
+    const dates = Array.from(range.by("day"));
+    bookedDates = bookedDates.concat(dates);
+  });
+
+  res.status(200).json({
+    success: true,
+    bookedDates,
+  });
+});
+
+export { newBooking, checkRoomBookingsAvailability, checkBookedDatesOfRoom };
